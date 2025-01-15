14e0a93a2ebdc37720f8850758f6768f79416b42
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-01-15 15:23:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import IncomeExpense from "./components/IncomeExpense";
 export default function App() {
   return (
     <div className="flex  justify-center m-10 h-full">
-      <div className=" w-[340px]  bg-slate-800 rounded-[1.5rem] p-4">
+      <div className=" w-[350px]  bg-slate-800 rounded-[1.5rem] p-4">
         <Header />
         <Balance />
         <IncomeExpense />
